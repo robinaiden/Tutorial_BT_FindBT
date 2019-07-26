8a76c33b946cfb301d05b7c1aa2bd784ad0df1c8
robinaiden <a10423041@yuntech.org.tw>
2019-07-27 01:58:33 +0800

    1.update string 2.新增bluetooth權限 3.增加bluetooth開啟功能

--- app/src/main/java/com/example/tutorial_bt_findbt/MainActivity.java
+++ app/src/main/java/com/example/tutorial_bt_findbt/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.tutorial_bt_findbt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         getView();
         setEvent();
+        FunBTInit();
 
     }
 
@@ -31,7 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void setEvent(){
 
-        button.setOnClickListener(this);
+        button.setOnClickListener(bluetoothOnClickListener);
+
+    }
+
+    private View.OnClickListener bluetoothOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            FunBTRegisterTheBroadcastReceiver();
+
+        }
+    };
+
+    //初始化藍芽設定: 檢查藍芽是否已打開
+    private BluetoothAdapter bluetoothAdapter;
+    private void FunBTInit(){
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(), R.string.null_BT, Toast.LENGTH_SHORT).show();
+        }
+
+        if (!bluetoothAdapter.isEnabled()){
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+        } else {
+            //搜尋藍芽設備
+        }
+
+    }
+
+    private void FunBTRegisterTheBroadcastReceiver(){
 
     }
 
